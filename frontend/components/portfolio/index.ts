1a17af5f1ec667289563955e247c68b4cd94b88a
Khanh Pham <khanh.phamdd@gmail.com>
2025-08-30 21:18:56 +0800

    feat: integrate HistoricalValueChart into portfolio page

---
+++ frontend/components/portfolio/index.ts
@@ -0,0 +1,5 @@
+export { PortfolioOverview } from './PortfolioOverview';
+export { PortfolioPieChart } from './PortfolioPieChart';
+export { ProtocolBreakdown } from './ProtocolBreakdown';
+export { TokenList } from './TokenList';
+export { HistoricalValueChart } from './HistoricalValueChart';
